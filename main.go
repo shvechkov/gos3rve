e30569a18135f0795fbdfe4f509ab3a29569a6b3
Alexey Shvechkov <alexey@shvechkov.com>
2024-03-24 19:44:18 -0400

    incremental - first stab at implementing mb, put, ls, multipart upload

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -38,13 +44,13 @@ func main() {
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		handleGetObject(w, r)
+		handleGetRequest(w, r)
 	case http.MethodPut:
-		handlePutObject(w, r)
+		handlePutRequest(w, r)
 	case http.MethodDelete:
-		handleDeleteObject(w, r)
+		handleDeleteRequest(w, r)
 	case http.MethodPost:
-		handleCreateBucket(w, r)
+		handlePostRequest(w, r)
 	case "LIST":
 		handleListBuckets(w, r)
 	default:
@@ -52,12 +58,12 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handleGetObject(w http.ResponseWriter, r *http.Request) {
+func handleGetRequest(w http.ResponseWriter, r *http.Request) {
 	// Extract bucket name and object key from URL
-	bucketName, objectKey := extractBucketAndKey(r.URL.Path, r.URL.RawQuery)
+	bucketName, objectKey := extractBucketAndKey(r)
 
 	if bucketName == "" {
-		listAllMyBuckets(w, r, bucketPath)
+		_ = listBuckets(w, r, bucketPath)
 		return
 	}
 
@@ -78,56 +84,42 @@ func handleGetObject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//If key/prefix  points to a dir -> return list of objects with a given prefix
 	if fstat.IsDir() {
 		listObjects(w, r, bucketPath, bucketName, objectKey)
 		return
 	}
 
-	// Read file content
-	fileContent, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		http.Error(w, "Failed to read object content", http.StatusInternalServerError)
+	getObject(w, r, filePath)
+}
+
+func handlePutRequest(w http.ResponseWriter, r *http.Request) {
+	// Extract bucket name and object key from URL
+	bucketName, objectKey := extractBucketAndKey(r)
+
+	//Create Bucket request  -  PUT with bucket name and w/o object
+	if bucketName != "" && objectKey == "" {
+		makeBucket(w, r, bucketName)
 		return
 	}
 
-	// Serve file content
-	w.Header().Set("Content-Type", http.DetectContentType(fileContent))
-	w.Write(fileContent)
-}
-
-func handlePutObject(w http.ResponseWriter, r *http.Request) {
-	// Extract bucket name and object key from URL
-	bucketName, objectKey := extractBucketAndKey(r.URL.Path, r.URL.RawQuery)
+	// Handling below the request to create/upload an opbject
 
 	// Check if bucket exists
 	bucketPath := filepath.Join(bucketPath, bucketName)
 	if _, err := os.Stat(bucketPath); os.IsNotExist(err) {
-		http.Error(w, "Bucket not found", http.StatusNotFound)
-		return
-	}
-
-	// Read object content from request body
-	objectContent, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read object content", http.StatusInternalServerError)
+		s3error(w, r, "The specified bucket does not exist", "NoSuchBucket", http.StatusNotFound)
 		return
 	}
 
 	// Write object content to file
 	filePath := filepath.Join(bucketPath, objectKey)
-	err = ioutil.WriteFile(filePath, objectContent, 0644)
-	if err != nil {
-		http.Error(w, "Failed to write object to disk", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "Object %s uploaded successfully to bucket %s", objectKey, bucketName)
+	putObject(w, r, filePath)
 }
 
-func handleDeleteObject(w http.ResponseWriter, r *http.Request) {
+func handleDeleteRequest(w http.ResponseWriter, r *http.Request) {
 	// Extract bucket name and object key from URL
-	bucketName, objectKey := extractBucketAndKey(r.URL.Path, r.URL.RawQuery)
+	bucketName, objectKey := extractBucketAndKey(r)
 
 	// Check if bucket exists
 	bucketPath := filepath.Join(bucketPath, bucketName)
@@ -156,22 +148,56 @@ func handleDeleteObject(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Object %s deleted successfully from bucket %s", objectKey, bucketName)
 }
 
-func handleCreateBucket(w http.ResponseWriter, r *http.Request) {
-	// Extract bucket name from URL
-	bucketName := strings.TrimPrefix(r.URL.Path, "/")
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
 
-	// Create bucket directory if it doesn't exist
+	// Extract bucket name and object key from URL
+	bucketName, objectKey := extractBucketAndKey(r)
+
+	// Check if bucket exists
 	bucketPath := filepath.Join(bucketPath, bucketName)
 	if _, err := os.Stat(bucketPath); os.IsNotExist(err) {
-		err := os.Mkdir(bucketPath, 0755)
-		if err != nil {
-			http.Error(w, "Failed to create bucket", http.StatusInternalServerError)
-			return
-		}
+		s3error(w, r, "The specified bucket does not exist", "NoSuchBucket", http.StatusNotFound)
+		return
 	}
 
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "Bucket %s created successfully", bucketName)
+	//Logics for handling Multipart uploads goes below
+
+	//CreateMultipartUpload
+	//https://docs.aws.amazon.com/AmazonS3/latest/API/API_CreateMultipartUpload.html
+	if r.URL.RawQuery == "uploads" {
+
+		var buffer bytes.Buffer
+		buffer.WriteString(
+			fmt.Sprintf(`
+			<InitiateMultipartUploadResult>
+				<Bucket>%s</Bucket>
+				<Key>%s</Key>
+				<UploadId>%s</UploadId>
+			</InitiateMultipartUploadResult>
+	`, bucketName, objectKey, strconv.FormatInt(time.Now().UnixNano(), 10)))
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(buffer.Bytes())
+		log.Printf(fmt.Sprintf("Multipart upload intiated for %s  (bucket: %s ; object: %s)", r.URL.Path, bucketName, objectKey))
+		return
+	}
+
+	//https://docs.aws.amazon.com/AmazonS3/latest/API/API_CompleteMultipartUpload.html
+	//CompleteMultipartUpload
+	pattern := `^uploadId=\d+$`
+	regexFinilizeUpload, err := regexp.Compile(pattern)
+	if err != nil {
+		fmt.Println("Error compiling regular expression:", err)
+		return
+	}
+
+	if regexFinilizeUpload.MatchString(r.URL.RawQuery) {
+		finilieMultipartUpload(w, r, bucketPath, objectKey)
+		return
+	}
+
+	s3error(w, r, "Malformed request", "InvalidArgument", http.StatusNotFound)
+
 }
 
 func handleListBuckets(w http.ResponseWriter, r *http.Request) {
@@ -202,13 +228,49 @@ func handleListBuckets(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonData)
 }
 
-func extractBucketAndKey(path string, rquery string) (string, string) {
-	parts := strings.SplitN(path[1:], "/", 2)
+func isMultiPartUpload(r *http.Request) (error, bool, string, string) {
+
+	// Parse the URI
+	parsedURL, err := url.Parse(r.URL.RequestURI())
+	if err != nil {
+		fmt.Println("Error parsing URI:", err)
+		return err, false, "", ""
+	}
+
+	// Get the query parameters
+	queryParams := parsedURL.Query()
+
+	// Iterate over the query parameters
+	for key, values := range queryParams {
+		for _, value := range values {
+			fmt.Printf("Parameter: %s, Value: %s\n", key, value)
+		}
+	}
+
+	uploadId := queryParams.Get("uploadId")
+	partNumber := queryParams.Get("partNumber")
+
+	var ret bool = false
+
+	if uploadId != "" {
+		ret = true
+	}
+
+	return nil, ret, uploadId, partNumber
+}
+
+func extractBucketAndKey(r *http.Request) (string, string) {
+	query := r.URL.RawQuery
+
+	parts := strings.SplitN(r.URL.Path[1:], "/", 2)
 	bucket := parts[0]
 	key := ""
+	if len(parts) > 1 {
+		key = parts[1]
+	}
 
-	if rquery != "" {
-		tokens := strings.Split(rquery, "&")
+	if key == "" && query != "" {
+		tokens := strings.Split(query, "&")
 		params := make(map[string]string)
 
 		for _, arg := range tokens {
--- s3ops.go
+++ s3ops.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
+
+func makeBucket(w http.ResponseWriter, r *http.Request, bucketName string) (err error) {
+
+	// Check if bucket exists
+	bucketPath := filepath.Join(bucketPath, bucketName)
+	if _, err = os.Stat(bucketPath); os.IsNotExist(err) {
+		if err = os.MkdirAll(bucketPath, 0755); err != nil {
+			s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+	return nil
+}
 
 // consult with this
 // https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html#RESTErrorResponses
@@ -33,7 +54,7 @@ func s3error(w http.ResponseWriter, r *http.Request, msg string, code string, ht
 }
 
 // https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListBuckets.html
-func listAllMyBuckets(w http.ResponseWriter, r *http.Request, localPath string) (err error) {
+func listBuckets(w http.ResponseWriter, r *http.Request, localPath string) (err error) {
 
 	f, err := os.Open(localPath)
 	if err != nil {
@@ -80,6 +101,144 @@ func listAllMyBuckets(w http.ResponseWriter, r *http.Request, localPath string) 
 `, s3user, userId))
 
 	w.Write(buffer.Bytes())
+	return nil
+}
+
+// Define a struct to represent the XML structure
+type XmlMultipartUploadPart struct {
+	PartNumber int    `xml:"PartNumber"`
+	ETag       string `xml:"ETag"`
+}
+
+type XmlCompleteMultipartUpload struct {
+	Parts []XmlMultipartUploadPart `xml:"Part"`
+}
+
+func finilieMultipartUpload(w http.ResponseWriter, r *http.Request, bucketPath string, objectKey string) error {
+
+	_, _, uploadId, _ := isMultiPartUpload(r)
+
+	objectContent, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+		log.Println("Error reading request data")
+		return err
+	}
+
+	// Parse the XML data
+	var data XmlCompleteMultipartUpload
+	err = xml.Unmarshal(objectContent, &data)
+	if err != nil {
+		fmt.Println("Error parsing XML:", err)
+		return err
+	}
+
+	dstFilePath := filepath.Join(bucketPath, objectKey)
+
+	dstFile, err := os.OpenFile(dstFilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return err
+	}
+	defer dstFile.Close()
+
+	for _, part := range data.Parts {
+		fmt.Printf("PartNumber: %d, ETag: %s\n", part.PartNumber, part.ETag)
+
+		srcFile := bucketPath + "/" + uploadId + "_" + strconv.FormatInt(int64(part.PartNumber), 10) + "_" + objectKey
+		// Open the binary file for reading
+
+		objectContent, err := os.ReadFile(srcFile)
+		if err != nil {
+			s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+			log.Println("CompleteMultipartUpload: failed to read from ", srcFile, " ", err.Error())
+			return err
+
+		}
+
+		// Append data to the file
+		_, err = dstFile.Write(objectContent)
+		if err != nil {
+			s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+			log.Println("CompleteMultipartUpload: Error while writing into file ", dstFilePath, " ", err.Error())
+			return err
+		}
+
+		err = os.Remove(srcFile)
+		if err != nil {
+			log.Printf("CompleteMultipartUpload: Error wile deleting %s , err: %s", srcFile, err.Error())
+		}
+
+	}
+
+	return nil
+}
+
+func putObject(w http.ResponseWriter, r *http.Request, filePath string) (err error) {
+
+	// Read object content from request body
+	objectContent, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+		log.Println("Error reading request data")
+		return
+	}
+
+	// Write object content to file
+	dirPath := filepath.Dir(filePath)
+	if err = os.MkdirAll(dirPath, 0755); err != nil {
+		s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+		log.Println("Error while creating parent directories")
+		return
+	}
+
+	//if it is a multipart upload , modify filePath
+	err, isMulti, uploadId, partNumber := isMultiPartUpload(r)
+	if isMulti {
+		filePath = filepath.Dir(filePath) + "/" + uploadId + "_" + partNumber + "_" + filepath.Base(filePath)
+		//filePath = filePath + "_" + uploadId + "_" + partNumber
+	}
+
+	if err = ioutil.WriteFile(filePath, objectContent, 0644); err != nil {
+		s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+		log.Println("Error while writing into file ", filePath, " ", err.Error())
+		return
+	}
+
+	hash := md5.New()
+	if _, err = hash.Write(objectContent); err != nil {
+		s3error(w, r, "InternalServerError", "InternalServerError", http.StatusInternalServerError)
+		log.Println("Error while calculating md5 ", err.Error())
+		return
+	}
+
+	hash_str := hex.EncodeToString(hash.Sum(nil))
+
+	w.Header().Set("ETag", hash_str)
+	w.WriteHeader(http.StatusCreated)
+
+	return nil
+}
+
+func getObject(w http.ResponseWriter, r *http.Request, filePath string) (err error) {
+
+	// Check if file exists
+	if _, err = os.Stat(filePath); os.IsNotExist(err) {
+		s3error(w, r, "The resource you requested does not exist", "NoSuchKey", http.StatusNotFound)
+		return
+	}
+
+	// Read file content
+	fileContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		http.Error(w, "Failed to read object content", http.StatusInternalServerError)
+		return
+	}
+
+	// Serve file content
+	w.Header().Set("Content-Type", http.DetectContentType(fileContent))
+	w.Write(fileContent)
+
 	return nil
 }
 
